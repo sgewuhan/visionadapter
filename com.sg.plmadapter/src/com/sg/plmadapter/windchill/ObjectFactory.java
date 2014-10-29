da4f54939e6a6a45f6ede8afc2a234bbbf0a1cb4
ycx <ycx@ccs>
2014-10-29 16:31:56 +0800

    Signed-off-by: ycx <ycx@ccs>

--- com.sg.plmadapter/src/com/sg/plmadapter/windchill/ObjectFactory.java
+++ com.sg.plmadapter/src/com/sg/plmadapter/windchill/ObjectFactory.java
@@ -25,12 +25,20 @@ import javax.xml.namespace.QName;
 public class ObjectFactory {
 
     private final static QName _CreateFolderResponse_QNAME = new QName("http://pm.webservice.integration.tmt.ext/", "createFolderResponse");
+    private final static QName _CreateDocumentResponse_QNAME = new QName("http://pm.webservice.integration.tmt.ext/", "createDocumentResponse");
     private final static QName _DeleteFolderResponse_QNAME = new QName("http://pm.webservice.integration.tmt.ext/", "deleteFolderResponse");
+    private final static QName _UpdateDocumentResponse_QNAME = new QName("http://pm.webservice.integration.tmt.ext/", "updateDocumentResponse");
     private final static QName _CreateFolder_QNAME = new QName("http://pm.webservice.integration.tmt.ext/", "createFolder");
+    private final static QName _DeleteDocument_QNAME = new QName("http://pm.webservice.integration.tmt.ext/", "deleteDocument");
+    private final static QName _MoveDocumentResponse_QNAME = new QName("http://pm.webservice.integration.tmt.ext/", "moveDocumentResponse");
     private final static QName _EditFolderResponse_QNAME = new QName("http://pm.webservice.integration.tmt.ext/", "editFolderResponse");
-    private final static QName _DeleteFolder_QNAME = new QName("http://pm.webservice.integration.tmt.ext/", "deleteFolder");
-    private final static QName _EditFolder_QNAME = new QName("http://pm.webservice.integration.tmt.ext/", "editFolder");
     private final static QName _Exception_QNAME = new QName("http://pm.webservice.integration.tmt.ext/", "Exception");
+    private final static QName _DeleteDocumentResponse_QNAME = new QName("http://pm.webservice.integration.tmt.ext/", "deleteDocumentResponse");
+    private final static QName _MoveDocument_QNAME = new QName("http://pm.webservice.integration.tmt.ext/", "moveDocument");
+    private final static QName _EditFolder_QNAME = new QName("http://pm.webservice.integration.tmt.ext/", "editFolder");
+    private final static QName _DeleteFolder_QNAME = new QName("http://pm.webservice.integration.tmt.ext/", "deleteFolder");
+    private final static QName _UpdateDocument_QNAME = new QName("http://pm.webservice.integration.tmt.ext/", "updateDocument");
+    private final static QName _CreateDocument_QNAME = new QName("http://pm.webservice.integration.tmt.ext/", "createDocument");
 
     /**
      * Create a new ObjectFactory that can be used to create new instances of schema derived classes for package: com.sg.plmadapter.windchill
@@ -48,27 +56,19 @@ public class ObjectFactory {
     }
 
     /**
+     * Create an instance of {@link MoveDocumentResponse }
+     * 
+     */
+    public MoveDocumentResponse createMoveDocumentResponse() {
+        return new MoveDocumentResponse();
+    }
+
+    /**
      * Create an instance of {@link EditFolderResponse }
      * 
      */
     public EditFolderResponse createEditFolderResponse() {
         return new EditFolderResponse();
-    }
-
-    /**
-     * Create an instance of {@link EditFolder }
-     * 
-     */
-    public EditFolder createEditFolder() {
-        return new EditFolder();
-    }
-
-    /**
-     * Create an instance of {@link DeleteFolder }
-     * 
-     */
-    public DeleteFolder createDeleteFolder() {
-        return new DeleteFolder();
     }
 
     /**
@@ -80,6 +80,14 @@ public class ObjectFactory {
     }
 
     /**
+     * Create an instance of {@link UpdateDocumentResponse }
+     * 
+     */
+    public UpdateDocumentResponse createUpdateDocumentResponse() {
+        return new UpdateDocumentResponse();
+    }
+
+    /**
      * Create an instance of {@link CreateFolder }
      * 
      */
@@ -88,11 +96,75 @@ public class ObjectFactory {
     }
 
     /**
+     * Create an instance of {@link DeleteDocument }
+     * 
+     */
+    public DeleteDocument createDeleteDocument() {
+        return new DeleteDocument();
+    }
+
+    /**
      * Create an instance of {@link CreateFolderResponse }
      * 
      */
     public CreateFolderResponse createCreateFolderResponse() {
         return new CreateFolderResponse();
+    }
+
+    /**
+     * Create an instance of {@link CreateDocumentResponse }
+     * 
+     */
+    public CreateDocumentResponse createCreateDocumentResponse() {
+        return new CreateDocumentResponse();
+    }
+
+    /**
+     * Create an instance of {@link UpdateDocument }
+     * 
+     */
+    public UpdateDocument createUpdateDocument() {
+        return new UpdateDocument();
+    }
+
+    /**
+     * Create an instance of {@link CreateDocument }
+     * 
+     */
+    public CreateDocument createCreateDocument() {
+        return new CreateDocument();
+    }
+
+    /**
+     * Create an instance of {@link MoveDocument }
+     * 
+     */
+    public MoveDocument createMoveDocument() {
+        return new MoveDocument();
+    }
+
+    /**
+     * Create an instance of {@link DeleteFolder }
+     * 
+     */
+    public DeleteFolder createDeleteFolder() {
+        return new DeleteFolder();
+    }
+
+    /**
+     * Create an instance of {@link EditFolder }
+     * 
+     */
+    public EditFolder createEditFolder() {
+        return new EditFolder();
+    }
+
+    /**
+     * Create an instance of {@link DeleteDocumentResponse }
+     * 
+     */
+    public DeleteDocumentResponse createDeleteDocumentResponse() {
+        return new DeleteDocumentResponse();
     }
 
     /**
@@ -105,12 +177,30 @@ public class ObjectFactory {
     }
 
     /**
+     * Create an instance of {@link JAXBElement }{@code <}{@link CreateDocumentResponse }{@code >}}
+     * 
+     */
+    @XmlElementDecl(namespace = "http://pm.webservice.integration.tmt.ext/", name = "createDocumentResponse")
+    public JAXBElement<CreateDocumentResponse> createCreateDocumentResponse(CreateDocumentResponse value) {
+        return new JAXBElement<CreateDocumentResponse>(_CreateDocumentResponse_QNAME, CreateDocumentResponse.class, null, value);
+    }
+
+    /**
      * Create an instance of {@link JAXBElement }{@code <}{@link DeleteFolderResponse }{@code >}}
      * 
      */
     @XmlElementDecl(namespace = "http://pm.webservice.integration.tmt.ext/", name = "deleteFolderResponse")
     public JAXBElement<DeleteFolderResponse> createDeleteFolderResponse(DeleteFolderResponse value) {
         return new JAXBElement<DeleteFolderResponse>(_DeleteFolderResponse_QNAME, DeleteFolderResponse.class, null, value);
+    }
+
+    /**
+     * Create an instance of {@link JAXBElement }{@code <}{@link UpdateDocumentResponse }{@code >}}
+     * 
+     */
+    @XmlElementDecl(namespace = "http://pm.webservice.integration.tmt.ext/", name = "updateDocumentResponse")
+    public JAXBElement<UpdateDocumentResponse> createUpdateDocumentResponse(UpdateDocumentResponse value) {
+        return new JAXBElement<UpdateDocumentResponse>(_UpdateDocumentResponse_QNAME, UpdateDocumentResponse.class, null, value);
     }
 
     /**
@@ -123,6 +213,24 @@ public class ObjectFactory {
     }
 
     /**
+     * Create an instance of {@link JAXBElement }{@code <}{@link DeleteDocument }{@code >}}
+     * 
+     */
+    @XmlElementDecl(namespace = "http://pm.webservice.integration.tmt.ext/", name = "deleteDocument")
+    public JAXBElement<DeleteDocument> createDeleteDocument(DeleteDocument value) {
+        return new JAXBElement<DeleteDocument>(_DeleteDocument_QNAME, DeleteDocument.class, null, value);
+    }
+
+    /**
+     * Create an instance of {@link JAXBElement }{@code <}{@link MoveDocumentResponse }{@code >}}
+     * 
+     */
+    @XmlElementDecl(namespace = "http://pm.webservice.integration.tmt.ext/", name = "moveDocumentResponse")
+    public JAXBElement<MoveDocumentResponse> createMoveDocumentResponse(MoveDocumentResponse value) {
+        return new JAXBElement<MoveDocumentResponse>(_MoveDocumentResponse_QNAME, MoveDocumentResponse.class, null, value);
+    }
+
+    /**
      * Create an instance of {@link JAXBElement }{@code <}{@link EditFolderResponse }{@code >}}
      * 
      */
@@ -132,12 +240,30 @@ public class ObjectFactory {
     }
 
     /**
-     * Create an instance of {@link JAXBElement }{@code <}{@link DeleteFolder }{@code >}}
+     * Create an instance of {@link JAXBElement }{@code <}{@link Exception }{@code >}}
      * 
      */
-    @XmlElementDecl(namespace = "http://pm.webservice.integration.tmt.ext/", name = "deleteFolder")
-    public JAXBElement<DeleteFolder> createDeleteFolder(DeleteFolder value) {
-        return new JAXBElement<DeleteFolder>(_DeleteFolder_QNAME, DeleteFolder.class, null, value);
+    @XmlElementDecl(namespace = "http://pm.webservice.integration.tmt.ext/", name = "Exception")
+    public JAXBElement<Exception> createException(Exception value) {
+        return new JAXBElement<Exception>(_Exception_QNAME, Exception.class, null, value);
+    }
+
+    /**
+     * Create an instance of {@link JAXBElement }{@code <}{@link DeleteDocumentResponse }{@code >}}
+     * 
+     */
+    @XmlElementDecl(namespace = "http://pm.webservice.integration.tmt.ext/", name = "deleteDocumentResponse")
+    public JAXBElement<DeleteDocumentResponse> createDeleteDocumentResponse(DeleteDocumentResponse value) {
+        return new JAXBElement<DeleteDocumentResponse>(_DeleteDocumentResponse_QNAME, DeleteDocumentResponse.class, null, value);
+    }
+
+    /**
+     * Create an instance of {@link JAXBElement }{@code <}{@link MoveDocument }{@code >}}
+     * 
+     */
+    @XmlElementDecl(namespace = "http://pm.webservice.integration.tmt.ext/", name = "moveDocument")
+    public JAXBElement<MoveDocument> createMoveDocument(MoveDocument value) {
+        return new JAXBElement<MoveDocument>(_MoveDocument_QNAME, MoveDocument.class, null, value);
     }
 
     /**
@@ -150,12 +276,30 @@ public class ObjectFactory {
     }
 
     /**
-     * Create an instance of {@link JAXBElement }{@code <}{@link Exception }{@code >}}
+     * Create an instance of {@link JAXBElement }{@code <}{@link DeleteFolder }{@code >}}
      * 
      */
-    @XmlElementDecl(namespace = "http://pm.webservice.integration.tmt.ext/", name = "Exception")
-    public JAXBElement<Exception> createException(Exception value) {
-        return new JAXBElement<Exception>(_Exception_QNAME, Exception.class, null, value);
+    @XmlElementDecl(namespace = "http://pm.webservice.integration.tmt.ext/", name = "deleteFolder")
+    public JAXBElement<DeleteFolder> createDeleteFolder(DeleteFolder value) {
+        return new JAXBElement<DeleteFolder>(_DeleteFolder_QNAME, DeleteFolder.class, null, value);
+    }
+
+    /**
+     * Create an instance of {@link JAXBElement }{@code <}{@link UpdateDocument }{@code >}}
+     * 
+     */
+    @XmlElementDecl(namespace = "http://pm.webservice.integration.tmt.ext/", name = "updateDocument")
+    public JAXBElement<UpdateDocument> createUpdateDocument(UpdateDocument value) {
+        return new JAXBElement<UpdateDocument>(_UpdateDocument_QNAME, UpdateDocument.class, null, value);
+    }
+
+    /**
+     * Create an instance of {@link JAXBElement }{@code <}{@link CreateDocument }{@code >}}
+     * 
+     */
+    @XmlElementDecl(namespace = "http://pm.webservice.integration.tmt.ext/", name = "createDocument")
+    public JAXBElement<CreateDocument> createCreateDocument(CreateDocument value) {
+        return new JAXBElement<CreateDocument>(_CreateDocument_QNAME, CreateDocument.class, null, value);
     }
 
 }
