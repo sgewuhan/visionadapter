6a0185917f06194bb8d64552a158db9f581edff1
ycx <ycx@ccs>
2014-11-07 14:40:49 +0800

    Signed-off-by: ycx <ycx@ccs>

---
+++ com.sg.plmadapter/src/com/sg/plmadapter/adapters/ModifyPhase.java
@@ -0,0 +1,24 @@
+package com.sg.plmadapter.adapters;
+
+import com.mobnut.db.model.PrimaryObject;
+import com.sg.business.model.IPLM_Object;
+import com.sg.plmadapter.windchill.PMWebservice;
+
+public class ModifyPhase extends WindchillSyncJob {
+
+	public ModifyPhase(PMWebservice windchill, PrimaryObject po) {
+		super(windchill, po);
+	}
+
+	@Override
+	protected void run(PMWebservice windchill, PrimaryObject po)
+			throws Exception {
+		
+	}
+
+	@Override
+	protected String getRequestCode() {
+		return IPLM_Object.REQUEST_CHANGE_PHASE;
+	}
+
+}
