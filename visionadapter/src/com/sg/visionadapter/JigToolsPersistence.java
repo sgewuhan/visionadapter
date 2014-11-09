c2c0bf3d0e253ae89b5a4b6c249860eef0afa49b
lcy <lcy@lcy.yaozheng.com>
2014-11-09 11:54:11 +0800

    Signed-off-by: lcy <lcy@lcy.yaozheng.com>

---
+++ visionadapter/src/com/sg/visionadapter/JigToolsPersistence.java
@@ -0,0 +1,11 @@
+package com.sg.visionadapter;
+
+
+public class JigToolsPersistence extends PersistenceService<PMJigTools>{
+
+	@Override
+	public String getCollectionName() {
+		return "document";
+	}
+
+}
