8f820e1f1355630f5c667ec10f8744dbf0d2b222
lcy <lcy@lcy.yaozheng.com>
2014-11-13 14:26:06 +0800

    Signed-off-by: lcy <lcy@lcy.yaozheng.com>

--- tmt/Windchill/src/ext/tmt/utils/Contants.java
+++ tmt/Windchill/src/ext/tmt/utils/Contants.java
@@ -11,32 +11,32 @@ public class Contants {
 	/*
 	 * 成品
 	 */
-	public static final String PRODUCTPART="com.plm.Product";  //成品
+	public static final String PRODUCTPART="com.yaozheng.Product";  //成品
 	
 	/*
 	 *半成品
 	 */
-	public static final String SEMIFINISHEDPRODUCT="com.plm.SemiFinishedProduct"; 
+	public static final String SEMIFINISHEDPRODUCT="com.yaozheng.SemiFinishedProduct"; 
 	/*
 	 *原材料
 	 */
-	public static final String RAWMATERIAL="com.plm.RawMaterial"; 
+	public static final String RAWMATERIAL="com.yaozheng.RawMaterial"; 
 	
 	/*
 	 * 客供件
 	 */
-	public static final String SUPPLYMENT="com.plm.GuestPart";
+	public static final String SUPPLYMENT="com.yaozheng.GuestPart";
 	
 	
 	/*
 	 * 包装材料
 	 */
-	public static final String PACKINGPART="com.plm.PackingMaterialPart";
+	public static final String PACKINGPART="com.yaozheng.PackingMaterialPart";
 	
 	/*
 	 * 备品备料
 	 */
-	public static final String TOOLPART="com.plm.ToolPart";
+	public static final String TOOLPART="com.yaozheng.ToolPart";
 	
 	
 	// 软属性
