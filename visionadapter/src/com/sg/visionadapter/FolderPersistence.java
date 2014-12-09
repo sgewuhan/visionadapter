1795b6a625e073792f4f3a0b114308bc42a9dd14
ycx <Administrator@Sc-201411141723>
2014-12-09 14:11:11 +0800

    提供数据库中未同步的数据_id Signed-off-by: ycx <Administrator@Sc-201411141723>

--- visionadapter/src/com/sg/visionadapter/FolderPersistence.java
+++ visionadapter/src/com/sg/visionadapter/FolderPersistence.java
@@ -1,8 +1,12 @@
 package com.sg.visionadapter;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.bson.types.ObjectId;
 
 import com.mongodb.BasicDBObject;
+import com.mongodb.DBCursor;
 import com.mongodb.DBObject;
 
 public class FolderPersistence extends PersistenceService<PMFolder> {
@@ -30,4 +34,19 @@ public class FolderPersistence extends PersistenceService<PMFolder> {
 		return null;
 	}
 
+	/**
+	 * plm_id为空的文件夹_id
+	 */
+	public List<String> getNoSyncFolderId() {
+		List<String> ids = new ArrayList<String>();
+		DBCursor find = collection.find(new BasicDBObject().append(
+				PMFolder.PLM_ID, null));
+		while(find.hasNext()) {
+			DBObject data = find.next();
+			ObjectId id = (ObjectId) data.get(VisionObject._ID);
+			String strId = id.toString();
+			ids.add(strId);
+		}
+		return ids;
+	}
 }
