52a23d246925799da232b8a2c08a8353865a38eb
yj <yj>
2015-01-16 16:16:42 +0800

    Signed-off-by: yj <yj>

--- visionadapter/src/com/sg/visionadapter/PMDeliverable.java
+++ visionadapter/src/com/sg/visionadapter/PMDeliverable.java
@@ -4,6 +4,8 @@ import org.bson.types.ObjectId;
 
 import com.mongodb.BasicDBObject;
 import com.mongodb.DBCollection;
+import com.mongodb.DBCursor;
+import com.mongodb.DBObject;
 import com.mongodb.WriteResult;
 
 public class PMDeliverable extends BasicDBObject implements IPersistenceService {
@@ -37,11 +39,26 @@ public class PMDeliverable extends BasicDBObject implements IPersistenceService 
 	}
 
 	public void initDeli(String codebasePath) {
-		DBCollection col = ModelServiceFactory.getInstance(codebasePath)
+		DBCollection docCol = ModelServiceFactory.getInstance(codebasePath)
 				.getCollection("document");
-		col.find(new BasicDBObject().append(
+		DBCollection deliCol = ModelServiceFactory.getInstance(codebasePath)
+				.getCollection("deliverable");
+		DBCursor docCursor = docCol.find(new BasicDBObject().append(
 				"IsSync",
 				new BasicDBObject().append("$nin", new Boolean[] {
 						Boolean.TRUE, Boolean.FALSE, null })));
+		while (docCursor.hasNext()) {
+			DBObject dbo = docCursor.next();
+			Object isSync = dbo.get("IsSync");
+			Object id = dbo.get("_id");
+			DBObject one = docCol.findOne(new BasicDBObject().append("plmid",
+					isSync));
+			Object idOld = one.get("_id");
+			deliCol.update(new BasicDBObject().append("document_id", id),
+					new BasicDBObject().append("$set",
+							new BasicDBObject().append("document_id", idOld)
+									.append("document_idold", id)), false, true);
+		}
+		docCursor.close();
 	}
 }
